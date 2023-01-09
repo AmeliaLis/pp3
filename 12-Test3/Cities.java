abb8bf17fc76ce7ef786ef770a1db96bbe7f0029
AmeliaLis <s221423@student.uek.krakow.pl>
2023-01-09 21:01:08 +0100

    task 5 fixes

--- 12-Test3/Cities.java
+++ 12-Test3/Cities.java
@@ -1,4 +1,4 @@
-public class Cities {
+/*public class Cities {
     private String[] array;
 
     Cities(String[] array){
@@ -20,5 +20,40 @@ public class Cities {
     public static void main(String[] args) {
         Cities c1 = new Cities(new String[] {"Warszawa","Sopot","Kielce","Szczecin"});
         System.out.println(c1.filter('S'));
+    }
+}
+*/
+import java.util.ArrayList;
+
+public class Cities {
+    String[] cities;
+
+    public Cities(String[] cities) {
+        this.cities = cities;
+    }
+
+    Cities filter(char c) {
+        ArrayList<String> cityList = new ArrayList<String>();
+        for (String city : cities){
+            if (c == city.charAt(0)){
+                cityList.add(city);
+            }
+        }
+
+        return new Cities(cityList.toArray(new String[cityList.size()]));
+    }
+
+    String cities (){
+        String cityString= "";
+        for (String city : cities){
+            cityString += city;
+        }
+        return cityString;
+    }
+
+    public static void main(String[] args) {
+        String[] myCities = {"Warszawa","Sopot","Kielce","Szczecin"};
+        Cities cities = new Cities(myCities);
+        System.out.println(cities.filter('S').cities());
     }
 }
